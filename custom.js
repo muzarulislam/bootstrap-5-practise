5337c3bfc9966274a56e2d4112f546a17347b31c
muzarulislam <developermuzarul@gmail.com>
2024-05-02 03:11:36 +0600

    some changes

--- custom.js
+++ custom.js
@@ -1,1 +1,3 @@
-// Toast
+// Tooltips 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
